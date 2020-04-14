f28361e46a313569982fb964755b2f00ff1dfdc6
John McNeill <jh_mcneill@yahoo.ca>
2020-04-13 21:54:15 -0400

    reading categories from server

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,11 +1,30 @@
 import React from "react";
+import { useUserQuery } from "../generated/graphql";
 
 interface ICategoriesProps {}
 
 const Categories: React.FC<ICategoriesProps> = (props) => {
+  const { data, loading } = useUserQuery();
+  let categories;
+
+  if (data && data.user) {
+    categories = data.user.categories;
+  }
+  console.log(categories);
+
   return (
     <div>
       <h1>Categories Component</h1>
+      {loading ? <div>Loading..</div> : null}
+      {categories && !loading ? (
+        <ul>
+          {categories.map((category) => (
+            <li key={category.name}>{category.name}</li>
+          ))}
+        </ul>
+      ) : (
+        <div>No categories</div>
+      )}
     </div>
   );
 };
